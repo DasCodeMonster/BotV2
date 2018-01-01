8b81b22d16268c247cc6fb8da123bf5e67384d6c
DasCodeMonster <pl.lkw.01@gmail.com>
2018-01-01 22:29:11 +0100

    permission command almost finished

--- commands/generic/permission.js
+++ commands/generic/permission.js
@@ -20,7 +20,7 @@ class Permission extends commando.Command {
             args:[{
                 key: 'cmdOrGrp',
 				label: 'command/group',
-				prompt: 'Which command or group would you like to reload?',
+				prompt: 'Which command or group would you like to change the permissions?',
 				validate: val => {
 					if(!val) return false;
 					const groups = this.client.registry.findGroups(val);
@@ -34,27 +34,7 @@ class Permission extends commando.Command {
 					`;
 				},
 				parse: val => this.client.registry.findGroups(val)[0] || this.client.registry.findCommands(val)[0]
-            },/*{
-                key: "command",
-                label: "command",
-                prompt: "you need to provide an command",
-                type: "command"
             }, {
-                key: "commandgroup",
-                label: "commandgroup",
-                prompt: "you need to give an commandgroup",
-                type: "commandgroup"
-            }, {
-                key: "commandname",
-                label: "commandname",
-                prompt: "PLACEHOLDER",
-                type: "commandname"
-            }, {
-                key: "option",
-                label: "option",
-                prompt: "you need to provide an option",
-                type: "option"
-            },*/ {
                 key: "group",
                 label: "role/user",
                 prompt: "you need to mention a role or user",
@@ -138,58 +118,10 @@ class Permission extends commando.Command {
             await this.client.provider.set(message.guild, args.cmdOrGrp.name, command);
         }
         else if (args.group.type === "role") {
-            // if(args.boolean == true){
-            //     if (command.true.length !== 0){
-            //         args.group.value.members.array().forEach((member, index, array)=>{
-            //             if(command.true.indexOf(member.user.id)>-1){
-            //                 return;
-            //             }
-            //             else {
-            //                 command.true.push(member.user.id);
-            //                 if(command.false.indexOf(member.user.id)>-1){
-            //                     command.false.splice(command.false.indexOf(member.user.id), 1);
-            //                 }
-            //             }
-            //         });
-            //     }
-            //     else {
-            //         args.group.value.members.array().forEach((member, index, array) => {
-            //             command.true.push(member.user.id);
-            //             if(command.false.indexOf(member.user.id)>-1){
-            //                 command.false.splice(command.false.indexOf(member.user.id), 1);
-            //             }
-            //         });
-            //     }
-            // }
-            // if(args.boolean === false){
-            //     if(command.false.length !== 0){
-            //         args.group.value.members.array().forEach((member, index, array)=>{
-            //             if(command.false.indexOf(member.user.id)>-1){
-            //                 return;
-            //             }
-            //             else {
-            //                 command.false.push(member.user.id);
-            //                 if(command.true.indexOf(member.user.id)>-1){
-            //                     command.true.splice(command.true.indexOf(member.user.id), 1);
-            //                 }
-            //             }
-            //         });
-            //     }
-            //     else {
-            //         args.group.value.members.array().forEach((member, index, array) => {
-            //             command.false.push(member.user.id);
-            //             if(command.true.indexOf(member.user.id)>-1){
-            //                 command.true.splice(command.true.indexOf(member.user.id), 1);
-            //             }
-            //         });
-            //     }
-            // }
-            // await this.client.provider.set(message.guild, args.cmdOrGrp.name, command);
-            // console.log(command);
             console.log(args.group.value);
             if(args.boolean === true){;
-                if(command.role.true.indexOf(args.group.value.id)){
-                    return;
+                if(command.role.true.indexOf(args.group.value.id)>-1){
+                    
                 }
                 else {
                     command.role.true.push(args.group.value.id);
@@ -199,8 +131,8 @@ class Permission extends commando.Command {
                 }
             }
             if(args.boolean === false){
-                if(command.role.false.indexOf(args.group.value.id)){
-                    return;
+                if(command.role.false.indexOf(args.group.value.id)>-1){
+                    
                 }
                 else {
                     command.role.false.push(args.group.value.id);
@@ -214,8 +146,8 @@ class Permission extends commando.Command {
         }
         else if (args.group.type === "channel"){
             if(args.boolean === true){;
-                if(command.channel.true.indexOf(args.group.value.id)){
-                    return;
+                if(command.channel.true.indexOf(args.group.value.id) >-1){
+                    
                 }
                 else {
                     command.channel.true.push(args.group.value.id);
@@ -225,8 +157,8 @@ class Permission extends commando.Command {
                 }
             }
             if(args.boolean === false){
-                if(command.channel.false.indexOf(args.group.value.id)){
-                    return;
+                if(command.channel.false.indexOf(args.group.value.id)>-1){
+                    
                 }
                 else {
                     command.channel.false.push(args.group.value.id);
@@ -243,25 +175,34 @@ class Permission extends commando.Command {
      * 
      * @param {Message} message 
      * @param {*} args 
+     * @returns {boolean}
      */
-    async hasPermission(message, args){
-        var command = await this.client.provider.get(message.guild, this.name, {true:[], false:[], channel: {true: [], false: []}, role:{true: [], false: []}})
-        if (message.member.hasPermission("ADMINISTRATOR")|| command.true.indexOf(message.author.id) != -1 || command.channel.true.indexOf(message.channel.id) || command.role.true.indexOf(message.member.roles.array())){
+    hasPermission(message, args){
+        var command = this.client.provider.get(message.guild, this.name, {true:[], false:[], channel: {true: [], false: []}, role:{true: [], false: []}})
+        // if (message.member.hasPermission("ADMINISTRATOR")|| command.true.indexOf(message.author.id) != -1 || command.channel.true.indexOf(message.channel.id)>-1 || role(message, command)){
+        if(message.member.hasPermission("ADMINISTRATOR")){
             return true;
         }
+        if(command.false.indexOf(message.author.id)>-1||command.channel.false.indexOf(message.channel.id)>-1||role(message, command)) return false;
         else {
-            return false;
+            return true;
         }
     }
 }
 /**
- * not finished yet
- * @param {*} command 
+ * @param {*} command
+ * @param {Message} message
+ * @returns {boolean}
  */
-function role(command) {
-    message.member.roles.array().forEach((role, index, array) => {
-        command.role.true.indexOf(role.id)
-
-    })
+function role(message, command) {
+    var ret;
+    message.member.roles.array().some((role, index, array) => {
+        if(command.role.true.indexOf(role.id) >-1) ret = true;return true;
+        if(index === array.length-1) {
+            ret = false;
+            return false;
+        }
+    });
+    return ret;
 }
 module.exports = Permission;
